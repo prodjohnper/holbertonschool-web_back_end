9c0d785ae9a18060943f692a165caa1588d3a17c
prodjohnper <jonathanperez9743@gmail.com>
2024-04-24 21:21:32 -0400

    Fix - Refactored code; git push

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -3,15 +3,13 @@
     Function task_wait_n that takes in an integer
     n and max_delay and returns a asyncio.Task.
 '''
-import asyncio
 from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''
         Function that takes in an integer n and
         max_delay and returns a asyncio.Task.
     '''
-    # Return the asyncio.Task object
-    return asyncio.create_task(wait_n(n, max_delay))
+    return await wait_n(n, max_delay)
