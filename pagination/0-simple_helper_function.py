7a210b64066620e7864a6a269e0c9b04a8544369
prodjohnper <JonathanPerez9743@gmail.com>
2024-04-29 22:24:40 -0400

    Add - Created README.md and helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+    Function index_range that takes two integer arguments page and page_size.
+'''
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+        Function index_range that takes
+        two integer arguments page and page_size.
+    '''
+    # Return a tuple of size two containing a start index and an end index.
+    return ((page - 1) * page_size, page * page_size)
