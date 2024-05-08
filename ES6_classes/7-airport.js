a7e2821c640d4cd02abfd14d5840f23bbdae015b
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-07 22:59:30 -0400

    Add - Created class Airport

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -2,14 +2,18 @@ export default class Airport {
   constructor(name, code) {
     // Check if name is a string
     if (typeof name === 'string') {
+      // Set the private property _name
       this._name = name;
     } else {
+      // Throw an error
       throw new Error('Name must be a string');
     }
     // Check if code is a string
     if (typeof code === 'string') {
+      // Set the private property _code
       this._code = code;
     } else {
+      // Throw an error
       throw new Error('Code must be a string');
     }
   }
