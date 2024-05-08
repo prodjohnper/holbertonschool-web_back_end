d2b47efcfac62c582b024258f6d7f92ad631548a
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-07 22:26:11 -0400

    Add - Created class Building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -9,19 +9,23 @@ export default class Building {
 
   // sqft getter and setter
   get sqft() {
+    // Return the value of _sqft
     return this._sqft;
   }
 
   set sqft(value) {
+    // Check if value is a number
     if (typeof value === 'number') {
       this._sqft = value;
     } else {
+        // Throw an error if value is not a number
       throw new Error('sqft must be a number');
     }
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
+  // Static method to return error message
+  static evacuationWarningMessage() {
+    // Throw an error with the message
     throw new Error(
       'Class extending Building must override evacuationWarningMessage',
     );
