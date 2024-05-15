f319ffaf09d0126b4b98d1cecbcb66890a9cc6fd
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-15 17:34:15 -0400

    Add - Created prototype that collectively resolves all promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,26 @@
+/*
+    3-all.js
+
+    Collectively resolve all promises
+*/
+
+// Import promises from utils.js
+const { uploadPhoto, createUser } = require('./utils');
+
+// Function that handles promises collectively
+export default function handleProfileSignup() {
+  // Return a promise
+  Promise.all([uploadPhoto(), createUser()])
+  // Handle the response
+    .then((results) => {
+    // Destructure the results
+      const [photo, user] = results;
+      // Log the results to the console
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+  // Handle the error
+    .catch(() => {
+    // Log the error to the console
+      console.error('Signup system offline');
+    });
+}
