303006db3ed980b61a49d6853ab60090e4aa619e
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-15 17:07:26 -0400

    Fix - Fixed output error

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -7,18 +7,22 @@
 // Function that returns a promise
 export default function handleResponseFromAPI(promise) {
   // Return a promise
-  promise.then(() => {
-    // Log the response to the console
-    console.log('Got a response from the API');
-    // Return a status code of 200 and a body of 'success'
-    return {
-      status: 200,
-      body: 'success',
-    };
-  }).catch((error) => {
-    // Log the error to the console
-    console.error('Error:', error.message);
-    // Return empty error obj
-    return new Error();
-  });
+  return promise
+    // Handle the response
+    .then(() => {
+      // Log the response to the console
+      console.log('Got a response from the API');
+      // Return a status code of 200 and a body of 'success'
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    // Handle the error
+    .catch(() => {
+      // Log the error to the console
+      console.log('Got a response from the API');
+      // Return empty error obj
+      return new Error();
+    });
 }
