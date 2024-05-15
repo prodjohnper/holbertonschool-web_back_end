cdf92394115cefda8de84535b6ec8873aa49befd
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-15 16:29:11 -0400

    Add - Created prototype that returns a promise. If true, it resolves passing an obj with 2 attrs. If false, it rejects with an error obj and message

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,23 @@
+/*
+    1-promise.js
+
+    Using given prototype, return a promise. The parameter is a boolean.
+*/
+
+// Function that returns a promise
+export default function getFullResponseFromAPI(success) {
+  // Return a promise
+  return new Promise((resolve, reject) => {
+    // If data is true, resolve the promise
+    if (success) {
+      // Resolve the promise with a status code of 200 and a body of 'Success'
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      // If data is false, reject the promise
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
