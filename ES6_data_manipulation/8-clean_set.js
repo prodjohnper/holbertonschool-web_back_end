1f48fa7d5ff35ac7ae91d2d11ad4265124ea746d
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-21 00:24:30 -0400

    Fix - Refactored code for output errors. Tried different approach

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -5,23 +5,20 @@
 */
 
 export default function cleanSet(set, startString) {
-  // Check if the first argument is a `Set`
-  if (!(set instanceof Set)) {
-    // Throw an error if the first argument is not a `Set`
-    throw new Error('First argument must be a Set');
+  // Check if the set is a `Set` obj and `startString` is a str
+  if (startString && typeof startString === 'string') {
+    // Iterate over the set and push the values that start with the `startString`
+    const values = [];
+    for (const item of set) {
+      // Check if the item starts with the `startString`
+      if (item.startsWith(startString)) {
+        // Push the item to the strings array
+        values.push(item.slice(startString.length));
+      }
+    }
+    // Return the strings array joined by a hyphen
+    return values.join('-');
   }
-
-  // Check if the second argument is a string
-  if (typeof startString !== 'string') {
-    // Throw an error if the second argument is not a string
-    throw new Error('Second argument must be a string');
-  }
-
-  // Filter the set values that start with the specific string
-  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
-  // Remove the specific string from the filtered values
-  const cleanedValues = filteredValues.map((value) => value.substring(startString.length));
-
-  // Return the values joined by a `-`
-  return cleanedValues.join('-');
+  // Return an empty string if the set is not a `Set` obj or `startString` is not a str
+  return '';
 }
