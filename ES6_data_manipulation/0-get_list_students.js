fd1b001beb9338fabe52dc4792c905731ea26f43
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-20 20:29:57 -0400

    Add - Created function that returns an array of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,14 @@
+/*
+    0-get_list_students.js
+
+    Function that returns an array of objects.
+*/
+
+export default function getListStudents() {
+  // Return an array of objs
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
